1df57e4e969f2901e110a9f3854fab1d92cbfa58
VadimGorbachevsky@gmail.com <shaersorrel@gmail.com>
2021-03-11 16:32:38 +0500

    Just a blank, nothing interesting

---
+++ BrowserHistory.py
@@ -0,0 +1,22 @@
+//Test exersise to PECAN AI
+
+class BrowserHistory:
+
+    def __init__(self, homepage):
+        super(, self).__init__();
+        self.homepage = homepage;
+        self.current_page = homrpage;
+
+    #def BrowserHistory(string homepage):
+        #self.homepage = homepage;
+
+    ~BrowserHistory(self):
+
+    def visit(self, url):
+        self.current_page;
+    def back(int steps):
+        return current_page;
+    def forward(int steps):
+        return current_page;
+
+test_case = BrowserHistory("google.com");
